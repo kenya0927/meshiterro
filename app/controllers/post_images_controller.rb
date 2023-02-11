933884e47d525b50dbf28d372654c31ba2c431fc
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-02-11 11:56:32 +0000

    [Finish]4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,7 +1,7 @@
 class PostImagesController < ApplicationController
 
   def new
-    @post_image= PostImage.new
+    @post_image = PostImage.new
   end
 
   def create
@@ -12,9 +12,17 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
   end
 
 
