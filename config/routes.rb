a3684b439ea6b64971b1c77bd2bdaaa9329016de
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-02-12 04:55:48 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as:"about"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
